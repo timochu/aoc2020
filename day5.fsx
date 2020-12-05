e2ec8ee8b7870826511a392b5e5fc71bed299e4c
Timo Vahtera <timo.vahtera@gmail.com>
2020-12-05 14:10:59 +0200

    Improve legibility of day 5 solution

--- day5.fsx
+++ day5.fsx
@@ -1,14 +1,18 @@
-let search (s : string) c = 
-    s 
-    |> Seq.fold (fun (lo, hi, _) x -> if x=c then (lo, hi-(hi+1-lo)/2, lo) else (lo+(hi+1-lo)/2, hi, hi)) (0, (pown 2 s.Length)-1, 0) 
-    |> fun (_,_,a) -> a
+let search (sequence : string) loChar upChar = 
+    let upperLimit = (pown 2 sequence.Length) - 1
+    let folder (lo, hi) c = 
+        let half = (hi - lo + 1) / 2
+        if c = loChar then (lo, hi - half)
+        elif c = upChar then (lo + half, hi)
+        else failwith "Unrecognized character"
+    sequence |> Seq.fold folder (0, upperLimit) |> fst
 
 let toId (s : string) =
-    let row = search s.[..6] 'F'
-    let column = search s.[7..] 'L'
+    let row = search s.[..6] 'F' 'B'
+    let column = search s.[7..] 'L' 'R'
     row * 8 + column
 
-let input = System.IO.File.ReadAllLines "inputs/day5.txt" |> Array.map toId |> Array.sortBy id
+let takenSeatIds = System.IO.File.ReadAllLines "inputs/day5.txt" |> Array.map toId
 
-printfn "highest seat: %i" (input |> Array.last)
-printfn "free seat: %i" ([Array.head input .. Array.last input] |> List.except input |> List.last)
+printfn "highest seat: %i" (takenSeatIds |> Array.max)
+printfn "free seat: %A" ([|Array.min takenSeatIds .. Array.max takenSeatIds|] |> Array.except takenSeatIds)
