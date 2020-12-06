8f8dee2dc9b58c1b94fa881d35aa32cdf3451762
Timo Vahtera <timo.vahtera@gmail.com>
2020-12-06 14:45:25 +0200

    Improve day 6 legibility by opening two types

--- day6.fsx
+++ day6.fsx
@@ -1,6 +1,8 @@
-let nl = System.Environment.NewLine
-let split (splitter : string) (s : string) = s.Split splitter
-let groups = System.IO.File.ReadAllText "inputs/day6.txt" |> split (nl + nl)
+open type System.Environment
+open type System.Char
 
-groups |> Array.sumBy (Seq.distinct >> Seq.where System.Char.IsLetter >> Seq.length)  |> printfn "Answer 1: %i"
-groups |> Array.sumBy (split nl >> Seq.map Set.ofSeq >> Set.intersectMany >> Set.count) |> printfn "Answer 2: %i"
+let split (splitter : string) (s : string) = s.Split splitter
+let groups = System.IO.File.ReadAllText "inputs/day6.txt" |> split (NewLine + NewLine)
+
+groups |> Array.sumBy (Seq.distinct >> Seq.where IsLetter >> Seq.length)  |> printfn "Answer 1: %i"
+groups |> Array.sumBy (split NewLine >> Seq.map Set.ofSeq >> Set.intersectMany >> Set.count) |> printfn "Answer 2: %i"
