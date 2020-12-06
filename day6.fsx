6293406d5bbdf382f9cda29dcb69cf0e8a4c4f74
Timo Vahtera <timo.vahtera@gmail.com>
2020-12-06 14:32:59 +0200

    Make use of custom split function in day 6

--- day6.fsx
+++ day6.fsx
@@ -1,5 +1,6 @@
 let nl = System.Environment.NewLine
-let groups = System.IO.File.ReadAllText "inputs/day6.txt" |> (fun x -> x.Split(nl + nl))
+let split (splitter : string) (s : string) = s.Split splitter
+let groups = System.IO.File.ReadAllText "inputs/day6.txt" |> split (nl + nl)
 
 groups |> Array.sumBy (Seq.distinct >> Seq.where System.Char.IsLetter >> Seq.length)  |> printfn "Answer 1: %i"
-groups |> Array.sumBy (fun group -> group.Split nl |> Seq.map Set.ofSeq |> Set.intersectMany |> Set.count) |> printfn "Answer 2: %i"
+groups |> Array.sumBy (split nl >> Seq.map Set.ofSeq >> Set.intersectMany >> Set.count) |> printfn "Answer 2: %i"
