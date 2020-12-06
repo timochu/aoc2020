6fdd3f5d8eb28e99d8fa236702aa2ded0cf6285e
Timo Vahtera <timo.vahtera@gmail.com>
2020-12-06 14:25:39 +0200

    Improve day 6 formatting

--- day6.fsx
+++ day6.fsx
@@ -1,5 +1,5 @@
-open System
-let groups = System.IO.File.ReadAllText "inputs/day6.txt" |> (fun x -> x.Split(Environment.NewLine + Environment.NewLine))
+let nl = System.Environment.NewLine
+let groups = System.IO.File.ReadAllText "inputs/day6.txt" |> (fun x -> x.Split(nl + nl))
 
-groups |> Array.sumBy (Seq.distinct >> Seq.where System.Char.IsLetter >> Seq.length)  |> printfn "%i"
-groups |> Array.sumBy (fun x -> x.Split(Environment.NewLine) |> Seq.map Set.ofSeq |> Set.intersectMany |> Set.count) |> printfn "%i"
+groups |> Array.sumBy (Seq.distinct >> Seq.where System.Char.IsLetter >> Seq.length)  |> printfn "Answer 1: %i"
+groups |> Array.sumBy (fun group -> group.Split nl |> Seq.map Set.ofSeq |> Set.intersectMany |> Set.count) |> printfn "Answer 2: %i"
