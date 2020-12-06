1dee4395bce5f97d3b8488a321029cdc02e3e75b
Timo Vahtera <timo.vahtera@gmail.com>
2020-12-06 14:02:15 +0200

    Fix unnecessary Array usage in day 6

--- day6.fsx
+++ day6.fsx
@@ -1,9 +1,9 @@
 let groups = System.IO.File.ReadAllText "inputs/day6.txt" |> (fun x -> x.Split(System.Environment.NewLine + System.Environment.NewLine))
 
-groups |> Array.sumBy (Seq.toArray >> Array.distinct >> Array.where System.Char.IsLetter >> Array.length)  |> printfn "%i"
+groups |> Array.sumBy (Seq.distinct >> Seq.where System.Char.IsLetter >> Seq.length)  |> printfn "%i"
 
 let countSameAnswers group =
-    let persons = group |> Array.tryFind (fst >> System.Char.IsWhiteSpace) |> Option.map snd |> Option.defaultValue 0 |> (+) 1
-    group |> Array.where (fun (_,y) -> y = persons) |> Array.length
+    let persons = group |> Seq.tryFind (fst >> System.Char.IsWhiteSpace) |> Option.map snd |> Option.defaultValue 0 |> (+) 1
+    group |> Seq.where (fun (_,y) -> y = persons) |> Seq.length
 
-groups |> Array.sumBy (Seq.toArray >> Array.countBy id >> countSameAnswers) |> printfn "%i"
+groups |> Array.sumBy (Seq.countBy id >> countSameAnswers) |> printfn "%i"
