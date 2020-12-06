63f114ea90012bcde219a6efdc38eb632dcd9c3c
Timo Vahtera <timo.vahtera@gmail.com>
2020-12-06 14:20:14 +0200

    Implement day 6 answer 2 using sets

--- day6.fsx
+++ day6.fsx
@@ -1,9 +1,5 @@
-let groups = System.IO.File.ReadAllText "inputs/day6.txt" |> (fun x -> x.Split(System.Environment.NewLine + System.Environment.NewLine))
+open System
+let groups = System.IO.File.ReadAllText "inputs/day6.txt" |> (fun x -> x.Split(Environment.NewLine + Environment.NewLine))
 
 groups |> Array.sumBy (Seq.distinct >> Seq.where System.Char.IsLetter >> Seq.length)  |> printfn "%i"
-
-let countSameAnswers group =
-    let persons = group |> Seq.tryFind (fst >> System.Char.IsWhiteSpace) |> Option.map snd |> Option.defaultValue 0 |> (+) 1
-    group |> Seq.where (fun (_,y) -> y = persons) |> Seq.length
-
-groups |> Array.sumBy (Seq.countBy id >> countSameAnswers) |> printfn "%i"
+groups |> Array.sumBy (fun x -> x.Split(Environment.NewLine) |> Seq.map Set.ofSeq |> Set.intersectMany |> Set.count) |> printfn "%i"
