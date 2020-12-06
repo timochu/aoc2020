8183bf35e153bee6338c99a0c12a0a079bb7143d
Timo Vahtera <timo.vahtera@gmail.com>
2020-12-06 14:54:14 +0200

    Day 6 formatting

--- day6.fsx
+++ day6.fsx
@@ -3,5 +3,5 @@ open type System.Environment
 let splitBy (splitter : string) (s : string) = s.Split splitter
 
 let groups = System.IO.File.ReadAllText "inputs/day6.txt" |> splitBy (NewLine + NewLine)
-groups |> Array.sumBy (Seq.distinct >> Seq.where System.Char.IsLetter >> Seq.length)  |> printfn "Answer 1: %i"
-groups |> Array.sumBy (splitBy NewLine >> Seq.map Set.ofSeq >> Set.intersectMany >> Set.count) |> printfn "Answer 2: %i"
+groups |> Array.sumBy (Seq.distinct >> Seq.where System.Char.IsLetter >> Seq.length)  |> printfn "%i"
+groups |> Array.sumBy (splitBy NewLine >> Seq.map Set.ofSeq >> Set.intersectMany >> Set.count) |> printfn "%i"
