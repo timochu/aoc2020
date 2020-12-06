63ff80cb4c876d1fe7dad2404557843ead0c20eb
Timo Vahtera <timo.vahtera@gmail.com>
2020-12-06 16:23:53 +0200

    Removed separate split helper function from day 6

--- day6.fsx
+++ day6.fsx
@@ -1,7 +1,5 @@
 open type System.Environment
 
-let splitBy (splitter : string) (s : string) = s.Split splitter
-
-let groups = System.IO.File.ReadAllText "inputs/day6.txt" |> splitBy (NewLine + NewLine)
+let groups = (System.IO.File.ReadAllText "inputs/day6.txt").Split(NewLine + NewLine)
 groups |> Array.sumBy (Seq.distinct >> Seq.where System.Char.IsLetter >> Seq.length)  |> printfn "%i"
-groups |> Array.sumBy (splitBy NewLine >> Seq.map Set.ofSeq >> Set.intersectMany >> Set.count) |> printfn "%i"
+groups |> Array.sumBy (fun x -> x.Split NewLine |> Seq.map Set.ofSeq |> Set.intersectMany |> Set.count) |> printfn "%i"
