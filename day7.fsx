daf2749fdaa23bed916bec87ada47ee4da429f83
Timo Vahtera <timo.vahtera@gmail.com>
2020-12-07 15:28:12 +0200

    Even better formatting for day 7

--- day7.fsx
+++ day7.fsx
@@ -1,15 +1,14 @@
 let input = System.IO.File.ReadAllLines("inputs/day7.txt") |> Array.toList
 
-let splitRules (str : string) = 
-    str.Split ([|" contain "; " bags"; " bag"; "."; ", "; "no other"|], System.StringSplitOptions.RemoveEmptyEntries)
-    |> Array.toList
-
-let flattenRule (rule : string list) =
-    rule.[1..] 
-    |> List.collect (fun color -> List.replicate (color.[0] |> System.Char.GetNumericValue |> int) (color.[2..])) 
-    |> fun colors -> rule.[0] :: colors
-
-let rules = input |> List.map (splitRules >> flattenRule)
+let rules = 
+    let splitRules (str : string) = 
+        str.Split ([|" contain "; " bags"; " bag"; "."; ", "; "no other"|], System.StringSplitOptions.RemoveEmptyEntries)
+        |> Array.toList
+    let flattenRule (rule : string list) =
+        rule.[1..] 
+        |> List.collect (fun color -> List.replicate (color.[0] |> System.Char.GetNumericValue |> int) (color.[2..])) 
+        |> fun colors -> rule.[0] :: colors
+    input |> List.map (splitRules >> flattenRule)
 
 let rec bagsFor color =
     let result = rules |> List.where (fun rule -> rule.[1..] |> List.exists ((=) color)) |> List.map List.head
