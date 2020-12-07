3184a3d5c0b062099f0baa059d5ccbba17ee46d1
Timo Vahtera <timo.vahtera@gmail.com>
2020-12-07 14:16:01 +0200

    Day 7 both solutions

--- day7.fsx
+++ day7.fsx
@@ -1,10 +1,18 @@
-let input = System.IO.File.ReadAllLines("inputs/day7.txt")
-let separators = [|" contain "; " bags"; " bag"; "."; ", "; "no other"; "1 "; "2 "; "3 "; "4 "; "5 "; |]
-let rules = input |> Seq.map (fun x -> x.Split(separators, System.StringSplitOptions.RemoveEmptyEntries))
+let input = System.IO.File.ReadAllLines("inputs/day7.txt") |> Array.toList
+let separators = [| " contain "; " bags"; " bag"; "."; ", "; "no other" |]
+let flattenRules (rules : list<string list>) =
+    rules |> List.map (fun x -> x.[1..] |> List.collect (fun y -> List.replicate (y.[0] |> System.Char.GetNumericValue |> int) (y.[2..])) |> (fun z -> [x.[0]] @ z))
+let rules =  input |> List.map (fun x -> x.Split(separators, System.StringSplitOptions.RemoveEmptyEntries) |> Array.toList) |> flattenRules
 
-let rec bagsFor bag rules =
-    let initial = rules |> Seq.where (fun (x : string []) -> x.[1..] |> Seq.exists ((=) bag)) |> Seq.map Seq.head |> Seq.toList
-    let recursive = initial |> Seq.collect (fun x -> bagsFor x rules) |> Seq.toList
+let rec bagsFor rules bag =
+    let initial = rules |> List.where (fun (x : string list) -> x.[1..] |> Seq.exists ((=) bag)) |> List.map Seq.head
+    let recursive = initial |> List.collect (bagsFor rules)
     initial @ recursive |> List.distinct
-    
-printfn "%i" (rules |> bagsFor "shiny gold" |> Seq.length)
+
+let rec bagsIn (rules : list<string list>) (bag : string) =
+    let initial = rules |> List.find (fun x -> x.[0] = bag) |> fun x -> x.[1..]
+    let recursive = initial |> List.collect (bagsIn rules)
+    initial @ recursive
+
+"shiny gold" |> bagsFor rules |> List.length |> printfn "%i"
+"shiny gold" |> bagsIn rules |> List.length |> printfn "%i"
