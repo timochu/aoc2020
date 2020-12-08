85f46b7a32676b1873887e9a3dbe77d4c7b977b5
Timo Vahtera <timo.vahtera@gmail.com>
2020-12-08 16:49:52 +0200

    day 8: rename command to instruction and remove mutable log

--- day8.fsx
+++ day8.fsx
@@ -1,43 +1,43 @@
 let input = System.IO.File.ReadAllLines("inputs/day8.txt")
 
-type Command = | Acc of int | Jmp of int | Nop of int
+type Instruction = | Acc of int | Jmp of int | Nop of int
 
-let toCommand (s : string) =
+let toInstruction (s : string) =
     let parts = s.Split ' '
     match parts.[0] with
     | "acc" -> Acc (parts.[1] |> int)
     | "jmp" -> Jmp (parts.[1] |> int)
     | "nop" -> Nop (parts.[1] |> int)
-    | _ -> failwith "Unrecognized command"
+    | _ -> failwith "Unrecognized instruction"
 
-let commands = input |> Array.map toCommand |> Array.indexed
+let instructions = input |> Array.map toInstruction |> Array.indexed
 
-let run subIndex (cmds : (int * Command) []) =
-    let mutable log = []
+let run substitutionIndex (instructions : (int * Instruction) []) =
+    let log = Array.zeroCreate instructions.Length
     let mutable accumulator = 0
     let mutable iterator = 0
 
-    let processCommand (commandIndex, command) =
-        log <- log @ [commandIndex]
-        match command with
+    let processCommand (index, instruction) =
+        Array.set log index (Some instruction)
+        match instruction with
         | Acc x -> 
             accumulator <- accumulator + x
             iterator <- iterator + 1
         | Jmp x -> 
-            if commandIndex = subIndex then iterator <- iterator + 1
+            if index = substitutionIndex then iterator <- iterator + 1
             else iterator <- iterator + x
         | Nop x -> 
-            if commandIndex = subIndex then iterator <- iterator + x
+            if index = substitutionIndex then iterator <- iterator + x
             else iterator <- iterator + 1
     
-    let isFailure () = log |> List.exists ((=) iterator)
-    let isFinalInstruction () = log |> List.exists ((=) (cmds |> Array.last |> fst))
+    let isFailure () = log.[iterator] |> Option.isSome
+    let isFinalInstruction () = iterator = instructions.Length-1
 
     while not (isFailure () || isFinalInstruction ()) do
-        processCommand cmds.[iterator]
+        processCommand instructions.[iterator]
 
     (isFinalInstruction(), accumulator)
 
-commands |> run 0 |> snd |> printfn "%i"
-[0 .. commands.Length] |> Seq.map (fun x -> commands |> run x) |> Seq.pick (fun (b, r) -> if b then Some r else None ) |> printfn "%i"
+instructions |> run 0 |> snd |> printfn "%i"
+[0 .. instructions.Length] |> Seq.map (fun x -> instructions |> run x) |> Seq.pick (fun (b, r) -> if b then Some r else None ) |> printfn "%i"
         
