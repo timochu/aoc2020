b730c139e2600f989e7c7be720d4bcd4341dff0a
Timo Vahtera <timo.vahtera@gmail.com>
2020-12-09 13:37:16 +0200

    day 9 clean up unnecessary parametrization

--- day9.fsx
+++ day9.fsx
@@ -1,11 +1,11 @@
 let input = System.IO.File.ReadAllLines("inputs/day9.txt") |> Array.map int64
 let preample = 25
 
-let isValid index (input: int64 array) =
+let isValid index =
     let previous = input.[index-preample .. index-1]
     (previous, previous) ||> Array.allPairs |> Array.map (fun (x,y) -> x+y) |> Array.exists ((=) input.[index])
 
-let answer = input.[preample..] |> Seq.mapi (fun i x -> (isValid (i+preample) input, x))
+let answer = input.[preample..] |> Seq.mapi (fun i x -> (isValid (i+preample), x))
              |> Seq.pick (fun (valid, value) -> if not valid then Some value else None) 
 
 let permutations startIndex = 
