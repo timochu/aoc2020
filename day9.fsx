38627c24c8fd2b8d08e50181c82285ab90b870c0
Timo Vahtera <timo.vahtera@gmail.com>
2020-12-09 10:38:14 +0200

    day 9: cleanup before solving answer 2

--- day9.fsx
+++ day9.fsx
@@ -1,10 +1,11 @@
 let input = System.IO.File.ReadAllLines("inputs/day9.txt") |> Array.map int64
 let preample = 25
 
-let isValid (index:int) (input: int64 array) =
+let isValid index (input: int64 array) =
     let previous = input.[index-preample .. index-1]
     (previous, previous) ||> Array.allPairs |> Array.map (fun (x,y) -> x+y) |> Array.exists ((=) input.[index])
 
-input.[preample..] |> Seq.mapi (fun i x -> (isValid (i+preample) input, x))
-             |> Seq.pick (fun (result, value) -> if not result then Some value else None ) 
-             |> printfn "%i"
+let answer = input.[preample..] |> Seq.mapi (fun i x -> (isValid (i+preample) input, x))
+             |> Seq.pick (fun (valid, value) -> if not valid then Some value else None) 
+
+printfn "%i" answer
