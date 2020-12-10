4ae3d7b7188c76e6f3a71284190d0bc09af9d778
Timo Vahtera <timo.vahtera@gmail.com>
2020-12-10 14:22:10 +0200

    day 10 answer 1

---
+++ day10.fsx
@@ -0,0 +1,12 @@
+let input = 
+    System.IO.File.ReadAllLines("inputs/day10.txt")
+    |> Array.toList 
+    |> List.map int 
+    |> List.sort
+    |> fun x -> [0] @ x @ [x |> List.max |> (+) 3]
+    |> Seq.pairwise
+
+let diffCount diff = 
+    input |> Seq.where (fun (x,y) -> y-x=diff) |> Seq.length
+
+printfn "%i" ((diffCount 1) * (diffCount 3))
