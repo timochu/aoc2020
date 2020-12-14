8a7e81beb26f2d1aa4d3b77255998a8fd4d5426e
Timo Vahtera <timo.vahtera@gmail.com>
2020-12-15 01:21:56 +0200

    day 13 potential answer 2 solution

--- day13.fsx
+++ day13.fsx
@@ -1,13 +1,22 @@
 let input =  System.IO.File.ReadAllLines("inputs/day13.txt")
-let timestamp = input.[0] |> int
-let buses = input.[1].Split ',' |> Seq.where ((<>) "x") |> Seq.map int |> Seq.toArray
+let timestamp = input.[0] |> int64
+let buses = input.[1].Split ',' |> Seq.indexed |> Seq.where (fun (_, x) -> x <> "x") |> Seq.map (fun (i,x) -> (int64 i, int64 x)) |> Seq.toArray
 
 let rec addWhileUnder x aggregate =
     if (aggregate+x) < timestamp then addWhileUnder x (aggregate+x)
     else (aggregate+x)
 
-let waitTimes = buses |> Seq.map (fun x -> addWhileUnder x 0 |> (-) timestamp)
+let waitTimes = buses |> Seq.map (fun (_, x) -> addWhileUnder x 0L |> (-) timestamp)
 let shortestWait = waitTimes |> Seq.max
-let bus = waitTimes |> Seq.findIndex ((=) shortestWait) |> fun x -> buses.[x]
+let bus = waitTimes |> Seq.findIndex ((=) shortestWait) |> fun x -> buses.[x] |> snd
 
 printfn "%i" (bus * (abs shortestWait))
+
+let mutable t = 0L
+let mutable found = false
+let checker (i, bus) = ((t+i) % bus) = 0L
+while not found do
+    t <- t+1L
+    found <- buses |> Seq.forall checker
+
+printfn "%i" t
