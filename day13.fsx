ecc47f4768717f189b452279b56251501c5b6796
Timo Vahtera <timo.vahtera@gmail.com>
2020-12-16 12:54:44 +0200

    day 13 improve answer 1 even more

--- day13.fsx
+++ day13.fsx
@@ -2,15 +2,14 @@ let input =  System.IO.File.ReadAllLines("inputs/day13.txt")
 let timestamp = input.[0] |> int64
 let buses = input.[1].Split ',' |> Seq.indexed |> Seq.where (fun (_, x) -> x <> "x") |> Seq.map (fun (i,x) -> (int64 i, int64 x)) |> Seq.toArray
 
-let rec addWhileUnder aggregate x =
-    if (aggregate+x) < timestamp then addWhileUnder (aggregate+x) x
-    else (aggregate+x)
+let rec waitTime aggregate x =
+    let result = aggregate + x
+    match result < timestamp with
+    | true -> waitTime result x
+    | false -> result
 
-let waitTimes = buses |> Seq.map (snd >> addWhileUnder 0L >> (-) timestamp)
-let shortestWait = waitTimes |> Seq.max
-let nextBus = waitTimes |> Seq.findIndex ((=) shortestWait) |> fun x -> buses.[x] |> snd
-
-printfn "%i" (nextBus * (abs shortestWait))
+let bus = buses |> Seq.map (fun (_ ,id) -> (id, id |> waitTime 0L |> (-) timestamp |> abs)) |> Seq.minBy snd
+printfn "%i" (bus |> fun (id, wait) -> id * wait)
 
 let busCount = buses.Length - 1
 let lcm a = a |> Seq.reduce (*) // only works for prime numbers
