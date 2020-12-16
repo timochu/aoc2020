932e59d283e4f4f214e9baa66a66cb094d452293
Timo Vahtera <timo.vahtera@gmail.com>
2020-12-16 14:30:59 +0200

    day 16 part 1 cleanup

--- day16.fsx
+++ day16.fsx
@@ -1,23 +1,23 @@
 open System
 open type System.Environment
-open type System.StringSplitOptions
 
 let input =  System.IO.File.ReadAllText("inputs/day16.txt").Split(NewLine + NewLine)
+
+let split (by : string seq) (s : string) = s.Split(by |> Seq.toArray, StringSplitOptions.RemoveEmptyEntries)
 
 let rules = 
     input 
     |> Seq.head 
-    |> fun x -> x.Split([|" "; NewLine|], RemoveEmptyEntries) 
+    |> split [" "; NewLine]
     |> Seq.where (fun x -> x.Contains '-')
-    |> Seq.map (fun x -> x.Split '-')
-    |> Seq.map (fun x -> (x |> Seq.head |> int, x |> Seq.last |> int))
+    |> Seq.map (split ["-"] >> fun x -> (x |> Seq.head |> int, x |> Seq.last |> int))
 
 let nearbyValues =
     input
     |> Seq.last
     |> fun x -> x.Split NewLine
     |> Seq.skip 1
-    |> Seq.collect (fun x -> x.Split([|","; NewLine|], RemoveEmptyEntries))
+    |> Seq.collect (split [","; NewLine])
     |> Seq.map int
 
 let isBetween value (min, max) = value >= min && value <= max
