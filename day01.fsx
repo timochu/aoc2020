e51246c1b5d5d3c61ce77732c72ad3b36f401a9b
Timo Vahtera <timo.vahtera@gmail.com>
2021-11-21 13:15:35 +0200

    Improve day 1 answer

--- day01.fsx
+++ day01.fsx
@@ -1,9 +1,9 @@
 let input = System.IO.File.ReadAllLines "inputs/day1.txt" |> Seq.map int
 
 let pairs = Seq.allPairs input input
-let answer = pairs |> Seq.filter (fun (x,y) -> x+y = 2020) |> Seq.head |> fun (x,y) -> x*y
+let answer = pairs |> Seq.pick (fun (x,y) -> if x+y = 2020 then Some (x*y) else None)
 printfn "%i" answer
 
 let triples = Seq.allPairs input pairs
-let answer2 = triples |> Seq.filter (fun (x, (y, z)) -> x+y+z = 2020) |> Seq.head |> fun (x, (y, z)) -> x*y*z
+let answer2 = triples |> Seq.pick (fun (x, (y, z)) -> if x+y+z = 2020 then Some (x*y*z) else None)
 printfn "%i" answer2
